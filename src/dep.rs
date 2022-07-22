940fcc144b7f0e0bd8c1286d9ae61ef833d1c80b
OliverUv <oliver.uvman@gmail.com>
2022-07-22 22:24:54 +0200

    Add dependency with error conversion

---
+++ src/dep.rs
@@ -0,0 +1,77 @@
+use snafu::prelude::*;
+use snafu::Error as CoreishError;
+
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+
+#[cfg(not(feature = "std"))]
+use ::alloc::boxed::Box;
+
+// #[cfg(not(feature = "std"))]
+// extern crate core;
+
+// #[cfg(not(feature = "std"))]
+// use core::Result;
+
+// XXX Getting an LSP error saying test_error is an unresolved import.
+use crate::{ErrorContainer, TestError, test_error};
+
+#[derive(Debug, Snafu)]
+enum DependencyError {
+
+    #[snafu(display("could not reticulate: {item}"))]
+    Reticulate {
+        item: String,
+    },
+
+    #[cfg(feature = "std")]
+    #[snafu(display("io error: {message}, from: {source}"))]
+    IO {
+        message: String,
+        source: std::io::Error,
+    },
+}
+
+impl Into<TestError> for DependencyError {
+    fn into(self) -> TestError {
+        use DependencyError as DE;
+        match self {
+
+            DE::Reticulate{..} => {
+                let error_container = ErrorContainer {
+                    source: Box::new(self) as _,
+                };
+                // XXX I wish there was a better way to do this
+                let r = Err(error_container);
+                let err:Result<(), TestError> = r.context(test_error::Generic);
+                err.unwrap_err()
+            },
+
+            #[cfg(feature = "std")]
+            DE::IO{message, source} => {
+                let r = Err(source);
+                let err:Result<(), TestError> = r.context(test_error::DepIO{ message });
+                err.unwrap_err()
+            },
+
+        }
+    }
+}
+
+pub fn call_dep() -> Result<(), TestError> {
+    inner().map_err(|e| e.into())
+}
+
+#[cfg(feature = "std")]
+fn inner() -> Result<(), DependencyError> {
+    let file = "/ASDASDFASDASDFASDF";
+    let _ = std::fs::read_to_string(file)
+        .context(IOSnafu{message: format!("could not read from file {}", file)})?;
+    Ok(())
+}
+
+#[cfg(not(feature = "std"))]
+fn inner() -> Result<(), DependencyError> {
+    ensure!(false, ReticulateSnafu{ item: "splines" });
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use snafu::prelude::*;
 // This is a reexport of std::error::Error on std, and on no_std it is
 // a trait very similar to std error, and also almost identical to the
 // traits in the core_error and core2 crates.
-use snafu::Error as CoreishError;
+pub use snafu::Error as CoreishError;
+
+mod dep;
+use dep::*;
 
 #[cfg(not(feature = "std"))]
 extern crate alloc;
@@ -36,11 +39,11 @@ impl CoreishError for ErrorContainer {
 }
 
 #[derive(Debug)]
-struct DependencyError {
+pub struct OwnError {
     pub message: String,
 }
 
-impl ::core::fmt::Display for DependencyError {
+impl ::core::fmt::Display for OwnError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.message)
     }
@@ -49,9 +52,9 @@ impl ::core::fmt::Display for DependencyError {
 // Snafu requires this specific trait to be implemented. Any home-rolled
 // Core-ish Error traits do not work, neither does simply impl:ing
 // Display and Debug
-impl CoreishError for DependencyError {}
+impl CoreishError for OwnError {}
 
-impl Into<ErrorContainer> for DependencyError {
+impl Into<ErrorContainer> for OwnError {
     fn into(self) -> ErrorContainer {
         ErrorContainer {
             source: Box::new(self),
@@ -60,14 +63,36 @@ impl Into<ErrorContainer> for DependencyError {
 }
 
 #[derive(Debug, Snafu)]
-enum TestError {
-    #[snafu(display("Generic error: {message}, from: {source}"))]
+#[snafu(visibility(pub(crate)))]
+#[snafu(module)]
+#[snafu(context(suffix(false)))]
+pub enum TestError {
+    #[snafu(display("Generic error: {source}"))]
     Generic {
+        source: ErrorContainer,
+    },
+
+    #[snafu(display("Specific error: {message}, from: {source}"))]
+    Specific {
+        message: String,
+        source: OwnError,
+    },
+
+    #[snafu(display("Dependency error: {message}, from: {source}"))]
+    DepGeneric {
         message: String,
         source: ErrorContainer,
     },
 
-    // // DOES NOT WORK IN NO_STD, FromString in Snafu is only impl'd in std
+    #[cfg(feature = "std")]
+    #[snafu(display("Dependency error: {message}, from: {source}"))]
+    DepIO {
+        message: String,
+        source: std::io::Error,
+    },
+
+    // // XXX Does not work in no_std, FromString in Snafu is only
+    // // impl'd in std
     //
     // #[snafu(whatever, display("Whatever-generic error: {message}"))]
     // WhateverGeneric {
@@ -76,28 +101,26 @@ enum TestError {
     //     source: Option<Box<dyn CoreishError + Send + Sync + 'static>>,
     // },
 
-    #[snafu(display("Specific error: {message}, from: {source}"))]
-    Specific {
-        message: String,
-        source: DependencyError,
-    },
 }
 
 fn main() {
 
-    let dep_err = DependencyError { message: "reported genericly".to_string() };
-    let dep_res:Result<(), DependencyError> = Result::Err(dep_err);
-    let generic_err = dep_res.map_err(|e| e.into()).context(GenericSnafu{message: "generic message"});
+    let own_err = OwnError { message: "reported genericly".to_string() };
+    let own_res:Result<(), OwnError> = Result::Err(own_err);
+    let generic_err = own_res.map_err(|e| e.into()).context(test_error::Generic);
     println!("G: {}", generic_err.unwrap_err());
 
-    // let dep_err = DependencyError { message: "i broke in some dependency".to_string() };
-    // let dep_res:Result<(), DependencyError> = Result::Err(dep_err);
-    // let whatever_generic_err:Result<(), TestError> = dep_res.whatever_context("whatever generic message");
+    // let own_err = OwnError { message: "reported via whatever".to_string() };
+    // let own_res:Result<(), OwnError> = Result::Err(own_err);
+    // let whatever_generic_err:Result<(), TestError> = own_res.whatever_context("whatever generic message");
     // println!("W: {}", whatever_generic_err.unwrap_err());
 
-    let dep_err = DependencyError { message: "reported specifically".to_string() };
-    let dep_res:Result<(), DependencyError> = Result::Err(dep_err);
-    let specific_err = dep_res.context(SpecificSnafu{message: "specific message" });
+    let own_err = OwnError { message: "reported specifically".to_string() };
+    let own_res:Result<(), OwnError> = Result::Err(own_err);
+    let specific_err = own_res.context(test_error::Specific{message: "specific message" });
     println!("S: {}", specific_err.unwrap_err());
+
+    let dep_res = call_dep();
+    println!("D: {}", dep_res.unwrap_err());
 
 }
